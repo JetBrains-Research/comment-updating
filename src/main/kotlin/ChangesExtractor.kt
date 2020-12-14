6392a2fe064460c121058b46926830e9e6d57bb3
Pavlov Ivan <pavlov200912@gmail.com>
2020-12-14 15:58:15 +0300

    fixed bug when doc and method simultaneous change leads to duplicate in change list

--- src/main/kotlin/ChangesExtractor.kt
+++ src/main/kotlin/ChangesExtractor.kt
@@ -38,42 +38,6 @@ class ChangesExtractor {
         Run.initGenerators()
     }
 
-    public fun extract(srcFile: File, dstFile: File): List<Sample> {
-        val src = JdtTreeGenerator().generate(InputStreamReader(srcFile.inputStream())).root
-        val dst = JdtTreeGenerator().generate(InputStreamReader(dstFile.inputStream())).root
-        val defaultMatcher: Matcher = Matchers.getInstance().matcher
-        val mappings: MappingStore = defaultMatcher.match(src, dst)
-        val editScriptGenerator: EditScriptGenerator = ChawatheScriptGenerator()
-        val actions = editScriptGenerator.computeActions(mappings)
-        val srcActions = mutableListOf<Pair<Int, Int>>()
-        val dstActions = mutableListOf<Pair<Int, Int>>()
-        for (action in actions) {
-            val range = action.node.pos to action.node.endPos
-            when(action) {
-                is Insert -> dstActions.add(range)
-                is Move -> null // Can't do anything, ignore this changes
-                is Delete -> srcActions.add(range)
-                is Update -> srcActions.add(range)
-                is TreeInsert -> srcActions.add(range)
-                is TreeDelete -> dstActions.add(range)
-            }
-        }
-        val intersectNames = listMethodsNames(dst as Tree).intersect(listMethodsNames(src as Tree)).toSet()
-        val srcChanged = extractChangedMethods(src as Tree, srcActions)
-        val dstChanged = extractChangedMethods(dst as Tree, dstActions)
-        val changed = srcChanged.union(dstChanged)
-        val samples = mutableListOf<Sample>()
-        for (node in changed) {
-            val name: String = (node.getMetadata("NAME") as? String) ?: ""
-            if (name in intersectNames) {
-                val srcPair = extractCodeComment(node, srcFile.readText())
-                val dstPair = extractCodeComment(node, dstFile.readText())
-                samples.add(Sample(srcPair.second, srcPair.first, dstPair.second, dstPair.first))
-            }
-        }
-        return samples
-    }
-
 
     // TODO: Catch SyntaxException from JdtTreeGenerator
     public fun extract(srcBytes: ByteArray, dstBytes: ByteArray): List<Sample> {
@@ -101,26 +65,29 @@ class ChangesExtractor {
         val intersectNames = listMethodsNames(dst as Tree).intersect(listMethodsNames(src as Tree)).toSet()
         val srcChanged = extractChangedMethods(src as Tree, srcActions)
         val dstChanged = extractChangedMethods(dst as Tree, dstActions)
+        val visitedMethods = HashSet<String>()
         val samples = mutableListOf<Sample>()
         for (srcNode in srcChanged) {
             val name: String = (srcNode.getMetadata("NAME") as? String) ?: ""
-            if (name in intersectNames) {
+            if (name in intersectNames && !visitedMethods.contains(name)) {
                 val dstNode = mappings.getDstForSrc(srcNode)
                 if (dstNode.type.name == "MethodDeclaration") {
                     val srcPair = extractCodeComment(srcNode, srcBytes.toString(Charsets.UTF_8))
                     val dstPair = extractCodeComment(dstNode as Tree, dstBytes.toString(Charsets.UTF_8))
                     samples.add(Sample(srcPair.second, srcPair.first, dstPair.second, dstPair.first))
+                    visitedMethods.add(name)
                 }
             }
         }
         for (dstNode in dstChanged) {
             val name: String = (dstNode.getMetadata("NAME") as? String) ?: ""
-            if (name in intersectNames) {
+            if (name in intersectNames && !visitedMethods.contains(name)) {
                 val srcNode = mappings.getSrcForDst(dstNode)
                 if (dstNode.type.name == "MethodDeclaration") {
                     val srcPair = extractCodeComment(srcNode as Tree, srcBytes.toString(Charsets.UTF_8))
                     val dstPair = extractCodeComment(dstNode, dstBytes.toString(Charsets.UTF_8))
                     samples.add(Sample(srcPair.second, srcPair.first, dstPair.second, dstPair.first))
+                    visitedMethods.add(name)
                 }
             }
         }
